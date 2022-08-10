17654575d42f279584f11dc309def2e11fdbe2e6
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-10 16:57:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+let events = "surprise";
+function writeCards(names) {
+  let messages = [];
+  for ( let i = 0; i < names.length; i++) {
+   messages[i] = (`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+  return messages;
+};
+console.log(writeCards())
+
+let numbers = [10, 9, 8,7,6,5,4,3,2,1,0]
+function countDown(numbers) {
+  let number = 10;
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+  return numbers;
+}
